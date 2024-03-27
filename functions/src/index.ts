e42c9cdca56b4cabc3ef22f171bb25e4d5c06b91
Chetan Padia <chetbox@users.noreply.github.com>
2024-03-27 09:29:50 +0000

    imagePng function does not read history

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -13,11 +13,11 @@ function zeroPad(n: number|string, padding: number): string {
   return ('0'.repeat(padding) + n).slice(-padding);
 }
 
-function colorIndexFrom(canvas: Canvas, x: number, y: number): number|undefined {
-  const xPath = zeroPad(x.toString(2), canvas.depth);
-  const yPath = zeroPad(y.toString(2), canvas.depth);
-  let square: Square<any>|number|undefined = canvas.canvas;
-  for (let i=0; i<canvas.depth; i++) {
+function colorIndexFrom(depth: Canvas['depth'], canvas: Canvas['canvas'], x: number, y: number): number|undefined {
+  const xPath = zeroPad(x.toString(2), depth);
+  const yPath = zeroPad(y.toString(2), depth);
+  let square: Square<any>|number|undefined = canvas;
+  for (let i=0; i<depth; i++) {
     square = square !== undefined && square[xPath[i] + yPath[i]] || undefined;
   }
   return square as number;
@@ -31,22 +31,28 @@ function setColor(png: PNG, x: number, y: number, color: PaletteColor) {
   png.data[index + 3] = color.a;
 }
 
-export const imagePng = functions.https.onRequest((request, response) => {
-  database.ref('canvas').child(IMAGE_ID).once('value')
-  .then((snapshot) => {
-    const canvas = snapshot.val() as Canvas;
-    const size = Math.pow(2, canvas.depth);
+export const imagePng = functions.https.onRequest(async (request, response) => {
+  try {
+    const [depthSnapshot, canvasSnapshot] = await Promise.all([
+      database.ref('canvas').child(IMAGE_ID).child('depth').once('value'),
+      database.ref('canvas').child(IMAGE_ID).child('canvas').once('value'),
+    ]);
+    const depth = depthSnapshot.val() as Canvas['depth'];
+    const canvas = canvasSnapshot.val() as Canvas['canvas'];
+
+    const size = Math.pow(2, depth);
     const image = new PNG({width: size, height: size});
     for (let y = 0; y < size; y++) {
       for (let x = 0; x < size; x++) {
-        const colorIndex = colorIndexFrom(canvas, x, y);
+        const colorIndex = colorIndexFrom(depth, canvas, x, y);
         if (colorIndex !== undefined) {
           setColor(image, x, y, Palette[colorIndex]);
         }
       }
     }
-    return image.pack();
-  })
-  .then((image) => image.pipe(response).type('image/png'))
-  .catch((error) => response.status(500).send(error))
+    const packedImage = image.pack();
+    packedImage.pipe(response).type('image/png')
+  } catch (error) {
+    response.status(500).send(error)
+  }
 });
